ab7593a7833da848a4c592085d3ad84d16af7ae5
ALKA <ALKA20MALIK>
2018-12-26 00:15:39 +0530

    26/12/2018 12 15 AM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,17 +12,11 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthGuard } from './auth-guard.service';
+import { AuthService } from './auth.service';
 
-const appRoutes:Routes=[
-  { path:'',component:HomeComponent},
-  { path:'users',component:UsersComponent},
-  { path:'users/:id/:name',component:UserComponent},
-  { path:'servers',component:ServersComponent},
-  { path:'servers/:id',component:ServerComponent},
-  { path:'servers/:id/edit',component:EditServerComponent},
-
-
-];
 
 @NgModule({
   declarations: [
@@ -32,15 +26,16 @@ const appRoutes:Routes=[
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService,AuthService,AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
